62e8208a0a637288a08b025f2a549cf92a35b11a
Gvasa <gvasa989@gmail.com>
2016-01-06 15:42:37 +0100

    small changes with connection to db

--- index.php
+++ index.php
@@ -1,12 +1,36 @@
 <html>
 	<head>
- 		<title>PHP Test</title>
+ 		<title>Music Lib</title>
 	</head>
 
 	<body>
  		<?php 
- 			echo '<p>hello world</p>'; 
- 			phpinfo();
- 		?> 
+			include 'dbconnect.php';
+
+			/* $sql = "INSERT INTO $tablename (songName, artist, year, genre)
+					VALUES ('klaus', 'untz', 1964, 'insane')";
+
+					if ($dbConn->query($sql) === TRUE) {
+					    echo "New record created successfully <br>";
+					} else {
+					    echo "Error: " . $sql . "<br>" . $conn->error;
+					} */
+			
+			$sqlQuery = "SELECT * FROM $tablename WHERE 1";
+			$result = $dbConn->query($sqlQuery);
+
+			if ($result->num_rows > 0) {
+			    // output data of each row
+			    while($row = $result->fetch_assoc()) {
+			        echo "id: " . $row["ID"]. " - Name: " . $row["songName"]. " " . $row["artist"]. " " . $row["genre"]. "<br>";
+			    }
+			} else {
+			    echo "0 results";
+			}
+			$dbh->close();
+		?> 
+
+
 	</body>
 </html>
+
